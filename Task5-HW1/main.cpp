039447cec19f3e6573045654f05fcbb363b55c12
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-29 14:42:30 +0300

    Initial Commit

---
+++ Task5-HW1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task5-HW1
+//
+//  Created by Никита on 3/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
